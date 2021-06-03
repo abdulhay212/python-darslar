cc81e779bbe8c74a582af77ba5e37b4d610d3e4b
abdulhay212 <85228247+abdulhay212@users.noreply.github.com>
2021-06-03 09:19:21 +0500

    Add files via upload

---
+++ 03-dars.py
@@ -0,0 +1,22 @@
+"""
+03/06/2021
+
+Dasturlash asoslari
+
+#04-dars: 03 PRINT(), SINTEKS VA ARIFMETIK AMALLAR
+
+Dasturchi: Abdulhay Yaqubov
+
+Web sahifa: https://python.sariq.dev
+"""
+print("\"Nexia\", \"Tico\", \"Dmas\" ko'rganlar qilar havas")
+#1 5 ning 4-darajasi
+print("5 ning 4-darajasi", 5**4, "ga teng")
+#2 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print("22 ni 4 ga bo'lganda", 22%4, "qoldiq qoladi")
+#3 Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("Tomonlari 125 ga teng bo'lgan kvadratning yuzi", 125**2, "ga teng,", "perimetri esa", 125*4, "ga teng")
+#4 Diametri 12 ga teng bo'lgan doiraning yuzini toping
+print('Diametri 12 ga teng bo\'lgan doiraning yuzi', 3.14*(12/2)**2, 'ga teng')
+#5 Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping
+print('Katetlari 6 va 7 bo\'lgan to\'g\'ri burchakli uchburchakning gipotenuzasi', (6**2+7**2)**0.5, 'ga teng')
