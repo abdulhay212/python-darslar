b6d8358462d1aa79863bc7c01afe61991c54b125
abdulhay212 <85228247+abdulhay212@users.noreply.github.com>
2021-06-03 14:07:37 +0500

    06-dars: Sonlar
    06-dars: Sonlar

---
+++ 06-dars.py
@@ -0,0 +1,30 @@
+"""
+03/06/2021
+
+Dasturlash asoslari
+
+#06-dars: Sonlar
+
+Dasturchi: Abdulhay Yaqubov
+
+Web sahifa: https://python.sariq.dev
+"""
+# Foydalanuvchi kiritgan sonning kvadrati va kubini konsolga chiqaruvchi dastur
+# son = int(input("Istalgan sonni kiriting:\n>>>"))
+# print(f"{son} ning kvadrati {son**2} ga teng")
+# print(f"{son} ning kubi {son**3} ga teng")
+
+# Foydalanuvchining yoshini so'rab, uning tug'ilgan yilini hisoblab, konsolga chiqaruvchi dastur
+# yosh = int(input("Yoshingiz nechchida:\n>>>"))
+# print(f"Siz {2021-yosh} yilda tug'ilgansiz")
+
+# Foydalanuvchidan ikki son kiritshni so'rab, kiritilgan sonlarning yig'indisi, ayirmasi, 
+# ko'paytmasi va bo'linmasini chiqaruvchi dastur
+
+son1 = int(input("1-sonni kiriting:\n>>>"))
+son2 = int(input("2-sonni kiriting:\n>>>"))
+print(f"{son1} + {son2} = {son1 + son2}")
+print(f"{son1} - {son2} = {son1 - son2}")
+print(f"{son1} * {son2} = {son1 * son2}")
+print(f"{son1} / {son2} = {son1 / son2}")
+
