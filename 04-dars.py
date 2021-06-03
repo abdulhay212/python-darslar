100211f82d64858690bda9686e11f00060d113fe
abdulhay212 <85228247+abdulhay212@users.noreply.github.com>
2021-06-03 10:34:24 +0500

    04-dars: Oz'garuvchilar
    #04-dars: Oz'garuvchilar

---
+++ 04-dars.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+03/06/2021
+
+Dasturlash asoslari
+
+#04-dars: Oz'garuvchilar
+
+@author: Abdulhay Yaqubov
+
+Web sahifa: https://python.sariq.dev
+"""
+# "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+matn = "Hello World!"
+print(matn)
+
+# xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+xabar = "1-xabar"
+# print(xabar)
+xabar = "2-xabar"
+print(xabar)
+
+# class den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
+# O'zgaruvchini class deb nomlash mumkin emas, sababi class bu maxsus kalit so'z.
+
+#Quyidagi kodni bajaring
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+
+print("Radusi", radius, "ga teng aylananing yuzi", aylana_yuzi, "ga teng")
